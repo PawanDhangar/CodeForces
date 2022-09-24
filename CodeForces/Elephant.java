8d4ebdbd50c4749221ed4fe48216733d484249da
PawanDhangar <96188459+PawanDhangar@users.noreply.github.com>
2022-09-24 13:45:49 +0530

    Update Elephant.java

--- CodeForces/Elephant.java
+++ CodeForces/Elephant.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
 
+/*
+Contributed by
+Pawan Dhangar
+*/
+
 public class Elephant {
+    
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
